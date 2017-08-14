8596530099c72309ed428726307d6962e8129992
Briana Nevarez <banevare@ucsd.edu>
2017-08-14 13:47:19 -0700

    ftoc function and tests

---
+++ tempFuncs.py
@@ -0,0 +1,13 @@
+Python 3.6.1 (default, Jun 26 2017, 12:58:08) 
+[GCC 4.4.7 20120313 (Red Hat 4.4.7-16)] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> def ftoc(fTemp):
+	return (fTemp -32)*(5.0/9.0)
+
+>>> def ftoc(fTemp):
+	return (fTemp -32)*(5.0/9.0)
+7
+SyntaxError: invalid syntax
+>>> def ftoc(fTemp):
+	return (fTemp -32)*(5.0/9.0)
+7
